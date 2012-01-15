0d71ec1efec5101c82a0af743a9b4f2673715077
Benson Kalahar <bensonk42@gmail.com>
2012-01-14 17:34:43 -0800

    Made streaming work nicely

--- pine_siskin.py
+++ pine_siskin.py
@@ -7,20 +7,28 @@ from urllib2 import HTTPError, URLError
 
 # Twitter home timeline URL
 url = "https://userstream.twitter.com/2/user.json"
+friends = []
 
-def parse_tweets(json_str):
-  tweets = json.loads(json_str)
-  for tweet in tweets:
+def handle_init(json_str):
+  global friends
+  friends = json.loads(json_str)['friends']
+
+def handle_tweets(json_str):
+  try: 
+    tweet = json.loads(json_str)
     info = { "user": tweet['user']['name'].encode('utf-8', 'ignore'), "time": tweet['created_at'], "text": tweet['text'].encode('utf-8', 'ignore') }
     print("{user} at {time}:\n    {text}\n".format(**info))
-
+  except:
+    # This is to handle unexpected characters more gracefully. Ideally we'd
+    # have some sort of logging facility here. 
+    pass
 
 client = TwitterClient(oauth_token, oauth_secret)
 try:
   tweet_stream = client.get(url)
+  handle_init(tweet_stream.readline())
   for line in tweet_stream:
-    print line.strip()
-    # parse_tweets(line)
+    handle_tweets(line)
 except HTTPError, e:
   print "The server couldn't fulfill the request."
   print 'Error code: ', e.code
--- streaming_twitter.py
+++ streaming_twitter.py
@@ -5,14 +5,11 @@ import urllib2 as urllib
 import socket
 from config import consumer_secret, consumer_key
 socket._fileobject.default_bufsize = 0
-debug = True
-
 
 class TwitterClient(object):
   def __init__(self, token, secret):
     self.consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
     self.token = oauth.Token(token, secret)
-
 
   def get(self, url):
     # Set parameters
@@ -28,17 +25,11 @@ class TwitterClient(object):
     oauth_req = oauth.Request(method="GET", url=url, parameters=params)
     signature_method = oauth.SignatureMethod_HMAC_SHA1()
     oauth_req.sign_request(signature_method, self.consumer, self.token, include_body_hash=False)
-    if debug:
-      print("Normalized Parameters:\n\t{}".format(oauth_req.get_normalized_parameters()))
 
     # Make a urllib request object and open the connection
     headers = oauth_req.to_header()
     headers['User-agent'] = "Pine Siskin/0.1"
     headers['Accept-Encoding'] = "*/*"
-    if debug:
-      print("Headers:")
-      for k in headers:
-        print("\t{}: {}".format(k,headers[k]))
     req = urllib.Request(url=url, headers=headers)
     connection = urllib.urlopen(req)
     return connection
