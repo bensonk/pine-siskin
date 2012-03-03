4a0772529c35d814e500a93394f711a352b2f622
bensonk <bensonk42@gmail.com>
2012-03-03 00:19:03 -0800

    Made skeleton for CLI posting interface

--- cli.py
+++ cli.py
@@ -7,13 +7,29 @@ from models import Tweet
 # Twitter home timeline URL
 url = "https://userstream.twitter.com/2/user.json"
 client = TwitterClient(oauth_token, oauth_secret)
-try:
-  def printer(t): print t
-  client.watch(url, printer)
-except HTTPError, e:
-  print "The server couldn't fulfill the request."
-  print 'Error code: ', e.code
-  print 'Body: ', e.read()
-except URLError, e:
-  print "We failed to reach a server."
-  print 'Reason: ', e.reason
+
+
+
+def print_timeline():
+  try:
+    def printer(t): print t
+    client.watch(url, printer)
+  except HTTPError, e:
+    print "The server couldn't fulfill the request."
+    print 'Error code: ', e.code
+    print 'Body: ', e.read()
+  except URLError, e:
+    print "We failed to reach a server."
+    print 'Reason: ', e.reason
+
+
+def tweet(text):
+  print "Not implemented yet. In the future we'd post a tweet with the following content:\n\t{}".format(text)
+  pass # TODO: implement this
+
+if __name__ == "__main__":
+  from sys import argv
+  if len(argv) == 1:
+    print_timeline()
+  else:
+    tweet(" ".join(argv[1:]))
